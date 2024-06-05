96eb829a40efd18eec374c2ce6d8676a04549c96
Jason Ullrich <jasonullrich@gmail.com>
2024-06-05 15:23:47 -0700

    fetch and display product data

--- src/components/ProductDetails.jsx
+++ src/components/ProductDetails.jsx
@@ -1,8 +1,25 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 
 function ProductDetails() {
+  const [product, setProduct] = useState();
+
+  const getDetails = async () => {
+    let response = await fetch('https://app-hrsei-api.herokuapp.com/api/fec2/hr-rfp/products', {
+      headers: {
+        Authorization: process.env.GH_TOKEN,
+      },
+    });
+    response = await response.json();
+    setProduct(response[0]);
+  };
+
+  useEffect(() => {
+    getDetails();
+  }, []);
   return (
-    <>Details</>
+    <>
+      <pre>{JSON.stringify(product, null, 2)}</pre>
+    </>
   );
 }
 
