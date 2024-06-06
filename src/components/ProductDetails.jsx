878f61c9b44f952eea52a7581c2d07566bdae1d4
Jason Ullrich <jasonullrich@gmail.com>
2024-06-06 14:34:30 -0700

    set reviews link to reflect actual data

--- src/components/ProductDetails.jsx
+++ src/components/ProductDetails.jsx
@@ -11,11 +11,20 @@ import '../styles/productdetails.css';
 function ProductDetails() {
   const [product, setProduct] = useState(null);
   const [styles, setStyles] = useState(null);
+  const [rating, setRating] = useState(null);
   const [selectedStyleID, setSelectedStyleID] = useState(null);
+  const [selectedSizeID, setSelectedSizeID] = useState(null);
 
   let selectedStyle;
+  let sizes;
   if (styles && selectedStyleID) {
     selectedStyle = styles.find((style) => style.style_id === selectedStyleID);
+    sizes = Object.entries(selectedStyle.skus).map((sku) => ({ sku: sku[0], size: sku[1].size }));
+  }
+
+  let maxQuantity;
+  if (selectedSizeID) {
+    maxQuantity = Math.min(selectedStyle.skus[selectedSizeID].quantity, 15);
   }
 
   const { productID, setProductID } = useContext(AppContext);
@@ -35,36 +44,49 @@ function ProductDetails() {
     }
   };
 
+  const getRating = async () => {
+    const response = await axios.get(`/reviews/meta?product_id=${productID}`);
+    if (response.data) {
+      const ratings = Object.entries(response.data.ratings).reduce((ratings, current) => ratings.concat(Array.from({ length: parseInt(current[1]) }, (v) => parseInt(current[0]))), []);
+      console.log(ratings)
+      const avgRating = (ratings.reduce((sum, current) => sum + current, 0) / ratings.length).toFixed(2);
+      console.log(avgRating, typeof avgRating);
+      setRating({average: avgRating, total: ratings.length})
+    }
+  };
+
   useEffect(() => {
     getDetails();
     getStyles();
+    getRating();
   }, [productID]);
 
   return (
     <>
-      {product
+      {product && selectedStyle
         ? (
           <section id="product-details">
             <ImageGallery selectedStyle={selectedStyle} />
-            <section>
-              * * * * *
-              <a href="#">Read all reviews</a>
-              <h3>{product.category}</h3>
-              <h1>{product.name}</h1>
-              <p>{product.default_price}</p>
-              <p>
-                <span>STYLE &gt;</span>
-                {' '}
-                selected style
-              </p>
-              <StyleSelector styles={styles} selectedStyleID={selectedStyleID} setSelectedStyleID={setSelectedStyleID} />
+            <section id="product-details-side">
+              <div>
+                ({rating ? rating.average : null})* * * * *
+                <a href="#">Read all {rating ? rating.total : null} reviews</a>
+                <h3>{product.category}</h3>
+                <h1>{product.name}</h1>
+                <p>{product.default_price}</p>
+              </div>
+              <StyleSelector styles={styles} selectedStyle={selectedStyle} setSelectedStyleID={setSelectedStyleID} />
               <form>
                 <div>
-                  <select onChange={(e) => setSelectedStyleID(parseInt(e.target.value))}>
-                    {styles ? styles.map((style) => <option key={style.style_id} value={style.style_id}>{style.name}</option>) : null}
+                  <select defaultValue="" onChange={(e) => setSelectedSizeID(parseInt(e.target.value))}>
+                    <option value="" disabled hidden>Select Size</option>
+                    {sizes.map((size) => <option key={size.sku} value={size.sku}>{size.size}</option>)}
                   </select>
-                  <select>
-                    <option value="1">1</option>
+                  <select defaultValue="" disabled={!selectedSizeID}>
+                    <option value="" disabled hidden>-</option>
+                    {Array.from({ length: maxQuantity }, (v, i) => i + 1).map((qty) => (
+                      <option key={qty} value={qty}>{qty}</option>
+                    ))}
                   </select>
                 </div>
                 <div>
--- src/components/StyleSelector.jsx
+++ src/components/StyleSelector.jsx
@@ -1,13 +1,19 @@
 import React from 'react';
 
-function StyleSelector({ styles, selectedStyleID, setSelectedStyleID }) {
+function StyleSelector({ styles, selectedStyle, setSelectedStyleID }) {
   return (
     <div id="style-selector">
+      <p>
+        <span>STYLE &gt;</span>
+        {selectedStyle.name}
+      </p>
+      <ul>
       {styles ? styles.map((style, i) => (
         <li key={style.style_id} onClick={() => setSelectedStyleID(style.style_id)}>
           <img src={style.photos[i].thumbnail_url} />
         </li>
       )) : null}
+      </ul>
     </div>
   );
 }
