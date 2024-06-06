57da9e01d7631d260da6d772f7c0d358dea6eada
Jason Ullrich <jasonullrich@gmail.com>
2024-06-06 10:21:18 -0700

    remove postcss-loader (until tailwind please)

--- webpack.config.js
+++ webpack.config.js
@@ -33,7 +33,7 @@ module.exports = {
       },
       {
         test: /\.css$/,
-        use: ['style-loader', 'css-loader', 'postcss-loader'],
+        use: ['style-loader', 'css-loader'],
       },
     ],
   },
