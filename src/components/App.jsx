d54de5dcc727612aeecaaefc1282ad45fa1caa25
Jason Ullrich <jasonullrich@gmail.com>
2024-06-05 19:14:43 -0700

    add context to provide global state to whole app

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,21 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 import ProductDetails from './ProductDetails';
 import RelatedProducts from './RelatedProducts';
 import Reviews from './Reviews';
 
+import AppContext from '../context/AppContext';
+
 function App() {
+  const [productID, setProductID] = useState(40344);
+
   return (
-    <>
+    <AppContext.Provider value={{ productID, setProductID }}>
+      {productID}
       <ProductDetails />
       <RelatedProducts />
       <Reviews />
-    </>
+    </AppContext.Provider>
   );
 }
 
--- src/components/ProductDetails.jsx
+++ src/components/ProductDetails.jsx
@@ -1,24 +1,30 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, useContext } from 'react';
+import axios from 'axios';
+
+import AppContext from '../context/AppContext';
 
 function ProductDetails() {
   const [product, setProduct] = useState();
 
+  const { productID, setProductID } = useContext(AppContext);
+
   const getDetails = async () => {
-    let response = await fetch('https://app-hrsei-api.herokuapp.com/api/fec2/hr-rfp/products', {
+    const response = await axios.get(`https://app-hrsei-api.herokuapp.com/api/fec2/hr-rfp/products/${productID}`, {
       headers: {
         Authorization: process.env.GH_TOKEN,
       },
     });
-    response = await response.json();
-    setProduct(response[0]);
+    setProduct(response.data);
   };
 
   useEffect(() => {
     getDetails();
-  }, []);
+  }, [productID]);
+
   return (
     <>
       <pre>{JSON.stringify(product, null, 2)}</pre>
+      <button onClick={() => setProductID(40345)}>Click</button>
     </>
   );
 }
