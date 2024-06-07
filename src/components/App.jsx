98fe938d47fc9cb22f4390e11e9f9ac919e6b918
Tyler Johnson <tylerj0937@gmail.com>
2024-06-06 21:53:02 -0700

    Formatting updates

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-
+import '../styles.css';
 import ProductDetails from './ProductDetails';
 import RelatedProducts from './RelatedProducts';
 import Reviews from './Reviews';
