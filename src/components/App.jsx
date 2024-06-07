fc03c5999bcec5d6494710d83d05ca015b4646e3
Jason Ullrich <jasonullrich@gmail.com>
2024-06-06 17:01:09 -0700

    add logo

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -7,15 +7,23 @@ import Reviews from './Reviews';
 import AppContext from '../context/AppContext';
 
 import '../styles/global.css';
+import Logo from '../images/atelierlogo.svg';
 
 function App() {
   const [productID, setProductID] = useState(40344);
+  const [cart, setCart] = useState([]);
 
   return (
-    <AppContext.Provider value={{ productID, setProductID }}>
-      <ProductDetails />
-      {/* <RelatedProducts /> */}
-      {/* <Reviews /> */}
+    <AppContext.Provider value={{ productID, setProductID, cart, setCart }}>
+      <header>
+        <Logo height={96} />
+        <span>Cart {cart.length > 0 ? `(${cart.length})` : null}</span>
+      </header>
+      <main>
+        <ProductDetails />
+        {/* <RelatedProducts /> */}
+        {/* <Reviews /> */}
+      </main>
     </AppContext.Provider>
   );
 }
