dbd1e6108be71073ce3bace5de2b6156e2cf3ee1
Jason Ullrich <jasonullrich@gmail.com>
2024-06-11 10:24:08 -0700

    Filter by search query

--- src/components/QnA.jsx
+++ src/components/QnA.jsx
@@ -1,4 +1,4 @@
-import React, { useContext } from 'react';
+import React, { useContext, useState } from 'react';
 
 import { FaPlus, FaMagnifyingGlass } from 'react-icons/fa6';
 import QnAItem from './QnAItem';
@@ -6,20 +6,24 @@ import QnAItem from './QnAItem';
 import AppContext from '../context/AppContext';
 
 function QnA() {
+  const [filter, setFilter] = useState('');
+
   const { store: { questions } } = useContext(AppContext);
+
+  const filteredQuestions = filter.length >= 3 ? questions.filter((question) => question.question_body.toLowerCase().includes(filter.toLowerCase())) : questions;
 
   console.log(questions);
 
   return (
     <section className="flex flex-col gap-6 p-4 text-neutral-600 w-full">
-      {questions ? (
+      {filteredQuestions ? (
         <>
           <h3>QUESTIONS & ANSWERS</h3>
           <div className="relative">
-            <input type="search" name="qna_search" placeholder="HAVE A QUESTION? SEARCH FOR ANSWERS..." className="form-input w-full" />
+            <input type="search" name="qna_search" onChange={(e) => setFilter(e.target.value)} placeholder="HAVE A QUESTION? SEARCH FOR ANSWERS..." className="form-input w-full" />
             <FaMagnifyingGlass size={20} className="absolute right-4 top-1/2 -translate-y-1/2 pointer-events-none" />
           </div>
-          {questions.map((question) => <QnAItem question={question} />)}
+          {filteredQuestions.map((question) => <QnAItem question={question} />)}
           <div className="flex gap-4">
             <button className="form-input">MORE ANSWERED QUESTIONS</button>
             <button className="form-input flex justify-between items-center gap-4">
