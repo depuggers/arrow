09bcb9dc32d1b3b429324527d0744514ae6b2b05
Jason Ullrich <jasonullrich@gmail.com>
2024-06-11 18:29:50 -0700

    Install MSW

--- src/tests/Helpful.test.js
+++ src/tests/Helpful.test.js
@@ -1,12 +1,13 @@
-import React from 'react'
-import {render, screen} from '@testing-library/react'
-import '@testing-library/jest-dom'
+import '@testing-library/jest-dom';
+import { describe, test, expect } from '@jest/globals';
+import React from 'react';
+import { render, screen } from '@testing-library/react';
 
-import Helpful from '../components/Helpful'
+import Helpful from '../components/Helpful';
 
 describe('Helpful', () => {
   test('Should render the children provided', () => {
-    render(<Helpful>Report</Helpful>)
-    expect(screen.getByText('Report')).toBeInTheDocument()
-  })
-})
+    render(<Helpful>Report</Helpful>);
+    expect(screen.getByText('Report')).toBeInTheDocument();
+  });
+});
