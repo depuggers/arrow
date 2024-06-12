79004b0ec73def1c805c637447a8303852865762
Tyler Johnson <tylerj0937@gmail.com>
2024-06-11 22:04:46 -0700

    Create convertStars test

--- src/tests/convertStars.test.js
+++ src/tests/convertStars.test.js
@@ -1,4 +1,25 @@
-import '@testing-library/jest-dom';
 import { describe, test, expect } from '@jest/globals';
-import React from 'react';
+import '@testing-library/jest-dom';
 import { render, screen } from '@testing-library/react';
+import convertStars from '../lib/convertStars';
+
+describe('convertStars', () => {
+  test('returns empty object when invoked without a valid object that contains a "ratings" property', () => {
+    expect(() => convertStars('test').toHaveReturnedWith({}));
+  });
+  test('logs "invadlid data" when invoked without an argument', () => {
+    expect(() => console.log(convertStars('test')).toEqual('invalid data'));
+  });
+});
+
+// const convertStars = (data) => {
+//   if (!data || !data.ratings) {
+//     console.error('invalid data');
+//     return {};
+//   }
+//   const stars = Object.fromEntries(Object.entries(data.ratings).map(([k, v]) => [`stars${k}`, parseInt(v)]));
+//   return stars;
+// };
+
+
+// export default convertStars;
