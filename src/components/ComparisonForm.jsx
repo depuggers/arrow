6f43736e7e0b28140298292bc56669140cdc1beb
llu99 <l.lu90704@gmail.com>
2024-06-11 22:01:39 -0400

    with one written test

--- src/components/ComparisonForm.jsx
+++ src/components/ComparisonForm.jsx
@@ -32,11 +32,11 @@ function ComparisonForm({ defaultProduct }) {
       combinedFeatures.push({
         feature: feature.value,
         defaultFeatureValue: '✔️',
-        currentFeatureValue: currentProductFeatures.find((cpf) => cpf.feature === feature.feature) ? '✔️' : '✖️',
+        currentFeatureValue: currentProductFeatures.find((cpf) => cpf.value === feature.value) ? '✔️' : '✖️',
       });
     });
     currentProductFeatures.forEach((feature) => {
-      if (!defaultProductFeatures.some((dpf) => dpf.feature === feature.feature)) {
+      if (!defaultProductFeatures.some((dpf) => dpf.value === feature.value)) {
         combinedFeatures.push({
           feature: feature.value,
           defaultFeatureValue: '✖️',
---
+++ src/lib/combineAllFeatures.js
@@ -0,0 +1,23 @@
+const combineAllFeatures = (defaultProductFeatures, currentProductFeatures) => {
+  const combinedFeatures = [];
+  defaultProductFeatures.forEach((feature) => {
+    combinedFeatures.push({
+      feature: feature.value,
+      defaultFeatureValue: '✔️',
+      currentFeatureValue: currentProductFeatures.find((cpf) => cpf.value === feature.value) ? '✔️' : '✖️',
+    });
+  });
+  currentProductFeatures.forEach((feature) => {
+    if (!defaultProductFeatures.some((dpf) => dpf.value === feature.value)) {
+      combinedFeatures.push({
+        feature: feature.value,
+        defaultFeatureValue: '✖️',
+        currentFeatureValue: '✔️',
+      });
+    }
+  });
+
+  return combinedFeatures;
+}
+
+export default combineAllFeatures;
