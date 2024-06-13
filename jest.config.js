685e44cd513fc5cc9d62a303e82f7bf0e0b5d11a
Jason Ullrich <jasonullrich@gmail.com>
2024-06-12 18:36:18 -0700

    Enable verbose testing

--- jest.config.js
+++ jest.config.js
@@ -4,5 +4,8 @@ module.exports = {
     '\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$': '<rootDir>/src/tests/mocks/files.js',
     '\\.svg$': '<rootDir>/src/tests/mocks/svg.js',
     '\\.css$': '<rootDir>/src/tests/mocks/styles.js',
-  }
-}
+  },
+  collectCoverage: true,
+  collectCoverageFrom: ['./src/components/**', './src/reducers/**', './src/lib/**', './src/hooks/**'],
+  verbose: true,
+};
