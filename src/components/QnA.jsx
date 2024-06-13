0d978ddf444363032d7a37e1b695b418707cfbcc
Jason Ullrich <jasonullrich@gmail.com>
2024-06-13 12:46:44 -0700

    Adjust conditional rendering

--- src/components/QnA.jsx
+++ src/components/QnA.jsx
@@ -18,8 +18,11 @@ function QnA() {
 
   const { store: { questions }, showModal } = useContext(AppContext);
 
-  const filteredQuestions = filter.length >= 3 ? questions.filter((question) => question.question_body.toLowerCase().includes(filter.toLowerCase())) : questions;
-  const sortedQuestions = filteredQuestions ? filteredQuestions.sort((a, b) => (a.question_helpfulness >= b.question_helpfulness ? -1 : 1)) : null;
+  const loading = !questions;
+
+  let filteredQuestions = [];
+  filteredQuestions = filter.length >= 3 ? questions.filter((question) => question.question_body.toLowerCase().includes(filter.toLowerCase())) : questions;
+  const sortedQuestions = filteredQuestions ? filteredQuestions.sort((a, b) => (a.question_helpfulness >= b.question_helpfulness ? -1 : 1)) : [];
 
   // console.log(questions);
 
@@ -37,28 +40,26 @@ function QnA() {
         maxHeight: document.documentElement.clientHeight,
       }}
     >
-      {sortedQuestions ? (
-        <>
-          <h3>QUESTIONS & ANSWERS</h3>
-          <div className="relative">
-            <input type="search" name="qna_search" onChange={(e) => setFilter(e.target.value)} placeholder="HAVE A QUESTION? SEARCH FOR ANSWERS..." className="form-input w-full" />
-            <FaMagnifyingGlass size={20} className="absolute right-4 top-1/2 -translate-y-1/2 pointer-events-none" />
-          </div>
-          <div
-            className={`flex flex-col gap-6 overflow-y-auto ${scrolling ? 'pr-6' : ''}`}
-            ref={questionsRef}
-          >
-            {sortedQuestions.slice(0, visibleQuestions).map((question) => <QnAItem key={question.question_id} filter={filter} question={question} />)}
-          </div>
-          <div className="flex gap-4">
-            {visibleQuestions < sortedQuestions.length ? <button className="form-input" onClick={() => setVisibleQuestions(visibleQuestions + 2)}>MORE ANSWERED QUESTIONS</button> : null}
-            <button className="form-input flex justify-between items-center gap-4" onClick={() => showModal(<AddQuestion />)}>
-              ADD A QUESTION
-              <FaPlus size={24} />
-            </button>
-          </div>
-        </>
+      <h3>QUESTIONS & ANSWERS</h3>
+      <div className="relative">
+        <input type="search" name="qna_search" onChange={(e) => setFilter(e.target.value)} placeholder="HAVE A QUESTION? SEARCH FOR ANSWERS..." className="form-input w-full" />
+        <FaMagnifyingGlass size={20} className="absolute right-4 top-1/2 -translate-y-1/2 pointer-events-none" />
+      </div>
+      {!loading ? (
+        <div
+          className={`flex flex-col gap-6 overflow-y-auto ${scrolling ? 'pr-6' : ''}`}
+          ref={questionsRef}
+        >
+          {sortedQuestions.slice(0, visibleQuestions).map((question) => <QnAItem key={question.question_id} filter={filter} question={question} />)}
+        </div>
       ) : null}
+      <div className="flex gap-4">
+        {visibleQuestions < sortedQuestions.length ? <button className="form-input" onClick={() => setVisibleQuestions(visibleQuestions + 2)}>MORE ANSWERED QUESTIONS</button> : null}
+        <button className="form-input flex justify-between items-center gap-4" onClick={() => showModal(<AddQuestion />)}>
+          ADD A QUESTION
+          <FaPlus size={24} />
+        </button>
+      </div>
     </section>
   );
 }
