db5c2e22f67bcb2f5fe4165074c7cb3edd8ed851
Jason Ullrich <jasonullrich@gmail.com>
2024-06-13 07:46:23 -0700

    Refactor data fetching

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -12,7 +12,9 @@ import AppContext from '../context/AppContext';
 
 import appReducer from '../reducers/appReducer';
 
-import calculateRating from '../lib/calculateRating';
+import {
+  getDetails, getStyles, getQuestions, getRating,
+} from '../lib/fetchers';
 
 import '../styles/global.css';
 
@@ -27,27 +29,11 @@ function App() {
     selectedImage: 0, selectedStyle: 0, selectedSKU: null, cart: JSON.parse(localStorage.getItem('cart')) ?? [], helpfulQs: JSON.parse(localStorage.getItem('helpfulQs')) ?? [], helpfulAs: JSON.parse(localStorage.getItem('helpfulAs')) ?? [], reportedAs: JSON.parse(localStorage.getItem('reportedAs')) ?? [],
   });
 
-  const fetchData = async () => {
-    const requests = [
-      axios.get(`/products/${productID}`),
-      axios.get(`/products/${productID}/styles`),
-      axios.get(`/qa/questions?product_id=${productID}&count=4`),
-      axios.get(`/reviews/meta?product_id=${productID}`),
-    ];
-    const responses = await Promise.all(requests);
-    dispatch({
-      type: 'setProductDetails',
-      payload: {
-        product: responses[0].data,
-        styles: responses[1].data.results,
-        questions: responses[2].data.results,
-        rating: calculateRating(responses[3].data),
-      },
-    });
-  };
-
   useEffect(() => {
-    fetchData();
+    getDetails(productID, dispatch);
+    getStyles(productID, dispatch);
+    getQuestions(productID, dispatch);
+    getRating(productID, dispatch);
   }, [productID]);
 
   return (
@@ -56,7 +42,7 @@ function App() {
     }}
     >
       <Header />
-      <main className="flex flex-col gap-6 items-center">
+      <main className="w-full flex flex-col gap-6 items-center">
         <Overview />
         <div className="flex flex-col gap-6 items-center w-[80%] pb-6">
           {/* <RelatedProducts /> */}
