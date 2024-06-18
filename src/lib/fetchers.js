fc0854030088730f9bc578a8836b88366ae05779
Jason Ullrich <jasonullrich@gmail.com>
2024-06-18 12:05:39 -0700

    Add error handling

--- src/lib/fetchers.js
+++ src/lib/fetchers.js
@@ -2,43 +2,59 @@ import axios from 'axios';
 import calculateRating from './calculateRating';
 
 export const getDetails = async (productID, dispatch) => {
-  const res = await axios.get(`/products/${productID}`);
-  dispatch({
-    type: 'setProductDetails',
-    payload: {
-      product: res.data,
-    },
-  });
+  try {
+    const res = await axios.get(`/products/${productID}`);
+    dispatch({
+      type: 'setProductDetails',
+      payload: {
+        product: res.data,
+      },
+    });
+  } catch (err) {
+    console.error(err);
+  }
 };
 
 export const getStyles = async (productID, dispatch) => {
-  const res = await axios.get(`/products/${productID}/styles`);
-  dispatch({
-    type: 'setProductDetails',
-    payload: {
-      styles: res.data.results,
-    },
-  });
+  try {
+    const res = await axios.get(`/products/${productID}/styles`);
+    dispatch({
+      type: 'setProductDetails',
+      payload: {
+        styles: res.data.results,
+      },
+    });
+  } catch (err) {
+    console.error(err);
+  }
 };
 
 export const getQuestions = async (productID, dispatch) => {
-  const res = await axios.get(`/qa/questions?product_id=${productID}&count=3333`);
-  dispatch({
-    type: 'setProductDetails',
-    payload: {
-      questions: res.data.results,
-    },
-  });
+  try {
+    const res = await axios.get(`/qa/questions?product_id=${productID}&count=3333`);
+    dispatch({
+      type: 'setProductDetails',
+      payload: {
+        questions: res.data.results,
+      },
+    });
+  } catch (err) {
+    console.error(err);
+  }
 };
 
 export const getRating = async (productID, dispatch) => {
-  const res = await axios.get(`/reviews/meta?product_id=${productID}`);
-  dispatch({
-    type: 'setProductDetails',
-    payload: {
-      rating: calculateRating(res.data),
-    },
-  });
+  try {
+    const res = await axios.get(`/reviews/meta?product_id=${productID}`);
+    dispatch({
+      type: 'setProductDetails',
+      payload: {
+        rating: calculateRating(res.data),
+      },
+    });
+  } catch (err) {
+    console.error(err);
+  }
 };
 
 export const getRelatedProducts = async (productID, dispatch) => {
