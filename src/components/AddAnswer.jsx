e7001eaaac05a97efab41d8a0f2eea3d2d38aa33
Jason Ullrich <jasonullrich@gmail.com>
2024-06-17 17:07:59 -0700

    Update for testing

--- src/components/AddAnswer.jsx
+++ src/components/AddAnswer.jsx
@@ -49,10 +49,10 @@ function AddAnswer({ question }) {
           <input type="email" name="email" placeholder="Example: jack@email.com" className="form-input" maxLength={60} required />
           <p className="text-sm text-neutral-400">For authentication reasons, you will not be emailed</p>
         </label>
-        {/* <div data-testid="answer-photos" className="w-full flex justify-center gap-2">
-          {photos.map((photo, i) => <img key={i} className="w-[96px] aspect-square object-cover" src={photo} alt="" />)}
-        </div> */}
-        {photos}
+        <div data-testid="answer-photos" className="w-full flex flex-col gap-2">
+          {photos}
+          {/* {photos.map((photo, i) => <img key={i} className="w-[96px] aspect-square object-cover" src={photo} alt="" />)} */}
+        </div>
         {/* <input type="file" name="photos" accept="image/png, image/jpeg" /> */}
         {photos.length < 5 ? <button type="button" onClick={addPhoto} className="form-input w-fit mx-auto">Add photo</button> : null}
         {/* <input type="hidden" name="question_id" value={question.question_id} /> */}
